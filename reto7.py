71dc705bcdcadc856b27dd7948308bc6ef406669
lcarvajalsa <110641945+lcarvajalsa@users.noreply.github.com>
2022-09-14 15:52:02 -0500

    Se confirma datos de ingreso

--- reto7.py
+++ reto7.py
@@ -3,42 +3,43 @@ from re import S
 print("si tu cuenta de correo es MISENA marca 1 o 2 si es personal")
 num1=int(input("Ingrese el primer número\n "))
 while num1==2:
-    rta1=input("¿Registrarse en el sena? S para si, N para no")
+    rta1=input("¿Registrarse en el sena? S para si, N para no \n" )
     if rta1=="S" or rta1=="s":
+        print("Ahora tienes un coreo MISENA \n")
         num1=1
     else:
         break
 if num1==1:
-            print("tu correo es misena")
+            print("Tu correo es misena")
             rta=input("¿Es correcto? escribe si de lo contrario no \n")
             if rta=="si" :
-                print("eres aprendiz del SENA")
+                print("Eres aprendiz del SENA")
             elif rta=="Si":
-                print("eres aprendiz del SENA")
+                print("Eres aprendiz del SENA")
             elif rta=="SI":
-                print("eres aprendiz del SENA")
+                print("Eres aprendiz del SENA")
             elif rta=="sI":
                 print("Eres aprendiz del SENA")
             else:
-                print("no se te olvide activar tu cuenta soysena")
+                print("No se te olvide activar tu cuenta soysena")
             materias=int(input("Cuantas materias quieres inscribir: \n"))
             for i in range(0,materias,1):
-                print ("Mas una materia inscritas \n")
-            print("tienes",materias," incritas")
+                print ("Materia inscrita satisfactoriamente \n")
+            print("Tienes",materias," incritas")
             repetir="s"
             diario=0
             while repetir=="S" or repetir=="s":
-                plata=int(input("Ingresa tus gartos unitarios del dia \n"))
+                plata=int(input("Ingresa tus gastos unitarios del dia \n"))
                 diario=diario+plata
                 if diario<=10000:
                     repetir=input("Desea ingresar más gastos S o N para salir \n")
                 else:
-                    print("te quedaste sin plata retira por nequi")
+                    print("Te quedaste sin plata retira por nequi")
                     break
             print=input(f"Solo sacase 10000 y te estas gastando {diario}")
         
 else:
-    print("tienes la oportunidad de ser parte del SENA")
+    print("Tienes la oportunidad de ser parte del SENA")
 
 
 
