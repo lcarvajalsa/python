0a65eea92e7119a5d9e227b77d62b7cc85d78deb
lcarvajalsa <110641945+lcarvajalsa@users.noreply.github.com>
2022-09-28 15:29:45 -0500

    definitiva retos

--- reto6.py
+++ reto6.py
@@ -1,16 +1,37 @@
-#captura de diez datos 
-#te identificas como hombre o mujer
-#cuantos hay de hombre o mujeres1
-print("te identificas como hombre o mujer \n")
-tipo=0
-h=0
-m=0
-for i in range(1,11,1):
-    genero=int(input("Ingrese uno para hombre o dos para mujer \n"))
-    if genero==1:
-        h=h+1
-    elif genero==2:
-        m=m+1
+from random import randint
+election=int(input("oprima 1 para ingresar el valor de su factura o oprima 2 para ingresar la cantidad de productos comprados \n"))
+if election==1:
+    compra=int(input("ingrese el valor de su compra \n"))
+else:
+    numproductos=int(input("ingrese el numero de productos a comprar "))
+    valorproducto=10000
+    compra=numproductos*valorproducto
+descuento=randint(1,4)
+if compra>50000:
+    print("salio escogido/a para participar en nuestra promocion de aniversario")
+    if descuento==1:
+        procedimiento=compra*0.10
+        total=compra-procedimiento
+        print("salio bolita roja, su descuento es del 10% en el valor de su factura, su total a pagar es",total)
+    elif descuento==2:
+        procedimiento=compra*0.30
+        total=compra-procedimiento
+        print("salio bolita azul, su descuento es de 30% en el valor de su factura,su total a pagar es",total)
+    elif descuento==3:
+        procedimiento=compra*0.50
+        total=compra-procedimiento
+        print("salio bolita amarilla, su descuento es de 50% en el valor de su factura,su total a pagar es ",total)
+    elif descuento==4:
+        print("salio bolita blanca, te llevas tu compra gratis")
     else:
-        print("no hacer nada")
-print(f"hombre {h} mujeres {m}")
+        print(f"el juego escogio{descuento}")
+else:
+    print("no fue escogido/a para participar en nuestra promocion, su total a pagar es ",compra)
+if descuento==1 or descuento==2 or descuento==3:
+    valorrecibido=int(input("igrese el valor con el que desea cancelar "))
+    cambio=valorrecibido-total
+    print ("su compra fue de ",total," productos me realizo el pago con ",valorrecibido," su cambio es ",cambio)
+else: 
+    descuento==4
+    print("ganaste")
+
--- reto9.py
+++ reto9.py
@@ -1,4 +1,30 @@
 lista1=[]
 tiempo=[]
+menort=60000
+registro="s"
+while registro=="s" or registro=="S":
+    lista1.append (input("Escriba el nombre de jugador\n"))
+    registro=(input("Desea registrar otro jugador s para si n para no\n"))
+print("Los competidores inscritos son:")
+for x in lista1:
+    print(x)
+print("Tres---\n Dos-----\n Uno-----\n Arranca")
+for i in lista1:
+    print("Ingese el tiempo en segundos de ", i)
+    tiempo.append(int(input()))
+
+for l1, l2 in zip(lista1, tiempo):
+    print(l1, l2)
+    if l2<menort:
+        menort=l2
+print (f"Tiempo menor {menort} segundos")
+nombre=(tiempo.index(menort))
+print (f"Nombre del ganador {lista1[nombre]}\n")
+
+  
+
+
+
+
 
 
